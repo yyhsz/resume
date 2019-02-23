23dd3cd51e3ef0e07743509e6ecaed7da5fc0272
yyhsz <591985864@qq.com>
2019-02-23 10:57:05 +0800

    添加轮播

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+//轮播
+var mySwiper = new Swiper ('.swiper-container', {
+
+    loop: true, // 循环模式选项
+    
+    // 如果需要分页器
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    
+    // 如果需要前进后退按钮
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    
+  })        
 //强制展现加载动画
 setTimeout(function () { siteWelcome.classList.remove('active') }, 0)
 
